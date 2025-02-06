6aeaa9008d5a4532632facdfc0d9eecda9d7b0a2
pohrp <23049929@myrp.edu.sg>
2025-02-06 14:10:57 +0800

    next commit

--- generatorapp/eslint.config.js
+++ generatorapp/eslint.config.js
@@ -19,13 +19,23 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': React,
+      '@typescript-eslint': tseslint,
+      'prettier': eslintPluginPrettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      "prettier/prettier":"error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
   },
 )
